195372ccc3cda2d95f2d079f24535a34360c9df7
032-shi <0324omitsu@gmail.com>
2021-08-23 22:24:39 +0900

    [add]練習問題3_1追加

---
+++ practice_3_1/practice_3_1.java
@@ -0,0 +1,9 @@
+public class practice_3_1 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 2;
+        if (x > y) {
+          System.out.println("xはyより大きい。");
+        }
+    }
+}
