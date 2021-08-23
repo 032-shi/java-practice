9e85459e7948aec2adc66872570db01be7c683a4
032-shi <0324omitsu@gmail.com>
2021-08-23 18:10:29 +0900

    [add]練習問題5追加

--- practice_4/practice_4.java
+++ practice_4/practice_4.java
@@ -3,8 +3,9 @@ public class practice_4 {
         for (int i = 1; i <= 9; i++) {
           System.out.println(i + "の段");
           for (int j = 1; j <= 9; j++) {
-              System.out.println(i * j);
-          }            
+              System.out.print((i * j) + " ");
+          }
+          System.out.println("");
         }
     }
 }
