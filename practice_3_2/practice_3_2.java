f207b4e623b2b8e89069bfc827bf22d9badaf015
032-shi <0324omitsu@gmail.com>
2021-08-24 18:39:06 +0900

    [add]練習問題3_2追加

---
+++ practice_3_2/practice_3_2.java
@@ -0,0 +1,11 @@
+public class practice_3_2 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        if (x > y) {
+          System.out.println(x);
+        } else {
+          System.out.println(y);
+        }
+    }
+}
