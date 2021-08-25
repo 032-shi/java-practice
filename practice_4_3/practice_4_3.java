5a50ee9634f6b6527a2c1793c0981c2a3c008bc5
032-shi <0324omitsu@gmail.com>
2021-08-25 13:41:00 +0900

    [add]練習問題4_3追加

---
+++ practice_4_3/practice_4_3.java
@@ -0,0 +1,9 @@
+public class practice_4_3 {
+    public static void main(String[] args) {
+        int x = 1;
+        for (int i = 2; i < 9; i++) {
+          x *= i;
+          System.out.println(x);
+        }
+    }
+}
