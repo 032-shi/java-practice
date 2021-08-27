3a6be2c7dae8d90a0b4f7048a39c54e1fe6b86cf
032-shi <0324omitsu@gmail.com>
2021-08-27 23:11:39 +0900

    [add]練習問題逆ピラミッド追加

--- practice_reverse_pyramid/practice_reverse_pyramid.java
+++ practice_reverse_pyramid/practice_reverse_pyramid.java
@@ -1,10 +1,10 @@
-public class practice_pyramid {
+public class practice_reverse_pyramid {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-          for (int j = 0; j < 5 - (i + 1); j++) {
+          for (int j = 0; j < i; j++) {
             System.out.print(" ");
           }
-          for (int k = 0; k < (i + 1) * 2 - 1; k++) {
+          for (int k = 0; k < (5 - i) * 2 - 1; k++) {
             System.out.print("*");
           }
           System.out.println("");
